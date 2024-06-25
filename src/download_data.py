1f1b889291e765123d2c1107f10aade7c5267367
Malin Ettenhofer <ettenhofer@uni-potsdam.de>
2024-06-25 20:59:03 +0200

    final skript for downloading the data

--- src/download_data.py
+++ src/download_data.py
@@ -1,49 +1,41 @@
 from landsatxplore.api import API
 from landsatxplore.earthexplorer import EarthExplorer
 
-# Login to API and EarthExplorer
+# login to API and EarthExplorer
 username = "RSE_P2_open"  # username for USGS account
 password = "jerqez-kujKes-qopra0"  # password for USGS account
 api = API(username, password)  # API instance for Log-In
 earth_explorer = EarthExplorer(username, password)  # EarthExplorer instance for Log-In
 
-# List of Locations (latitude, longitude) in Saudi-Arabia
-locations = [ 
-    (24.77, 46.74)]  # Riad
-    #(21.49, 39.19),  # Jeddah
-    #(21.39, 39.86),  # Mecca
-    #(24.47, 39.61)]  # Medina
+# coordinates for polygon defining the chosen area around Haradh
+coordinates = [(24.5646, 49.0965), (24.5046, 49.4907), (24.2269, 49.4591), (24.0176, 49.4501), (23.9354, 49.3671), 
+               (23.7137, 49.3753), (23.4935, 49.2579), (23.2956, 49.4501), (23.1479, 49.440), (22.9887, 49.5126), 
+               (22.8445, 49.2737), (22.9230, 49.1226), (23.0696, 49.1364), (23.1883, 48.8589), (23.4506, 48.8644), 
+               (23.5917, 48.9771), (23.8959, 48.6530), (24.2920, 48.6530), (24.6046, 48.8727)]
 
-# Search for data in all locations
-for latitude, longitude in locations:
-    # Search within two different datasets and time periods
-    """
-    Landsat 5 TM Collection 2 was active from 1984-2013. Landsat 7 ETM+ Collection 2 started 1999 and is still active. For both datasets Level 2 
-    was chosen as it includes additional corrections to remove atmospheric effects. The irrigation project started in the 1970s (surely since 1976) 
-    but the first satellite only launched later.
-    """
-    for dataset, start_date, end_date in [('landsat_tm_c2_l2', '1985-01-01', '1999-12-31'), ('landsat_etm_c2_l2', '2000-01-01', '2020-12-31')]:
-        scenes = api.search(
-            dataset=dataset,
-            latitude=latitude,
-            longitude=longitude,
-            start_date=start_date,
-            end_date=end_date,
-            max_cloud_cover=10)  # maximum cloud coverage of 10% to assure clear detection
-            # bbox=[16.6, 34.5, 32.3, 55.7])  # coordinates of bounding box to cover Saudi-Arabia fully
-        
-        print(f'{len(scenes)} scenes for the location ({latitude}, {longitude}) were found. \n [{dataset}, {start_date}, {end_date}]')
-        for scene in scenes:
-            print(scene['acquisition_date'].strftime('%Y-%m-%d'))
-            # download the found scenes
-            earth_explorer.download(scene['entity_id'], output_dir='./data')
-            """
-            2018-05-31 Landsat 7 ETM+ Collection 2 Level 2
-            landsat_product_id 'LE07_L2SP_166043_20180531_20200829_02_T1' (failed with dataset id 1 of 3. Re-trying with the next one.) = 477M
-            entity_id 'LE71660432018151NPA00' (failed with dataset id 1 of 2. Re-trying with the next one.) = 288M ||| worked
-            display_id 'LE07_L2SP_166043_20180531_20200829_02_T1' (not tried)
-            landsat_scene_id 'LE71660432018151NPA00' (not tried)
-            """
+# defining the bounding box out of the coordinates
+latitudes = [coordinate[0] for coordinate in coordinates]
+longitudes = [coordinate[1] for coordinate in coordinates]
+bounding_box = [min(longitudes), min(latitudes), max(longitudes), max(latitudes)] # [48.653, 22.8445, 49.5126, 24.6046]
+
+# Search for data within two different datasets for the time period 2015-01-01 until 2020-12-31
+"""
+We chose the Landsat 8/9 Collection 2, that started in 2013 and is still active. For this satellite there are 439 scenes found for the chosen area and time period.
+For the dataset Level 2 was chosen as it includes additional corrections to remove atmospheric effects.
+Landsat 5 and 9 were/are active previously or afterwards our chosen timeslot. 
+Landsat 7 ETM+ Collection 2 has some technical issues since 2019 and due to data storage space we decided to only choose one dataset.
+"""
+scenes = api.search(
+    max_results = 1000, 
+    dataset = 'landsat_ot_c2_l2', 
+    start_date = '2015-01-01', 
+    end_date = '2020-12-31', 
+    max_cloud_cover = 10, # maximum cloud coverage of 10% to assure clear detection
+    bbox = bounding_box)
+
+# downloading each found scene
+for scene in scenes:
+    earth_explorer.download(scene['landsat_product_id'], output_dir='./data')
 
 # Logout of API and EarthExplorer
 api.logout()
